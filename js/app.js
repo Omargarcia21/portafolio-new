f9b3949c7ef0f21888e990b61c92ddeb1d1f8773
Omar Garcia <omar.garcia.21@hotmail.com>
2025-05-08 19:30:54 -0600

    Portafolio

--- js/app.js
+++ js/app.js
@@ -3,27 +3,16 @@ const menuBtn = document.getElementById('menu-btn');
 const navList = document.getElementById('nav-list');
 
 menuBtn.addEventListener('click', () => {
-    navList.classList.toggle('active');  // Muestra/oculta la lista
-    menuBtn.classList.toggle('active');  // Gira el ícono
+    navList.classList.toggle('active');
+    menuBtn.classList.toggle('active');
 });
 
 // Función para copiar al portapapeles
-function copyToClipboard(event, text) {
-    navigator.clipboard.writeText(text)
-        .then(() => {
-            showCopyMessage();
-        })
-        .catch((err) => console.error('Error al copiar: ', err));
-        event.preventDefault();
-}
-
-// Función para mostrar el mensaje de confirmación
-function showCopyMessage() {
-    const msg = document.getElementById('copy-msg');
-    msg.classList.add('show');
-
-    // Oculta el mensaje después de 2 segundos
-    setTimeout(() => {
-        msg.classList.remove('show');
-    }, 2000);
-}
+navigator.clipboard.writeText("Estr")
+.then(() => {
+  console.log('Contenido copiado al portapapeles');
+  /* Resuelto - texto copiado al portapapeles con éxito */
+},() => {
+  console.error('Error al copiar');
+  /* Rechazado - fallo al copiar el texto al portapapeles */
+});
